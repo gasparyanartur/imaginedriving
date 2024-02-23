7429bf94b4d09d84037ef37f47151057c3a4db89
Ruiqi Qiu <qiuruiqi1991@gmail.com>
2024-02-23 16:19:53 +0100

    wip

--- src/benchmark.py
+++ src/benchmark.py
@@ -6,12 +6,13 @@ from collections.abc import Iterable
 
 from src.data import load_img_paths_from_dir
 
-metric_names_mapping = {"psnr": PeakSignalNoiseRatio(data_range=1.0),
-                "ssim": StructuralSimilarityIndexMeasure(data_range=1.0),
-                "lpips": LearnedPerceptualImagePatchSimilarity(et_type='squeeze')
-                }
+metric_names_mapping = {
+    "psnr": PeakSignalNoiseRatio(data_range=1.0),
+    "ssim": StructuralSimilarityIndexMeasure(data_range=1.0),
+    "lpips": LearnedPerceptualImagePatchSimilarity(net_type='squeeze')
+}
 
-def benchmark_img_to_img(img_pred_path: pl.Path, img_gt_path: pl.Path, metric_names: Iterable) -> dict():
+def benchmark_img_to_img(img_pred_path: pl.Path, img_gt_path: pl.Path, metric_names: Iterable[str]) -> dict[str, float]:
     
     img_pred = load_img_paths_from_dir(img_pred_path)
     img_gt = load_img_paths_from_dir(img_gt_path)
