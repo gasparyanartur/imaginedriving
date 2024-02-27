775d4ba4b7a9c35fcfc477c616361d119672cba0
Artur Gasparyan <gasparyanartur99@gmail.com>
2024-02-27 14:32:39 +0100

    Fix diffusion

--- src/data.py
+++ src/data.py
@@ -4,14 +4,10 @@ import json
 import torch
 from torch import Tensor
 from torchvision.transforms import v2 as tvtf2
-import torchvision as tv
+import torchvision 
 
 
-base_img_pipeline = tvtf2.Compose([
-    tvtf2.ToDtype(torch.float32, scale=True)
-]
-
-)
+base_img_pipeline = tvtf2.Compose([tvtf2.ToDtype(torch.float32, scale=True)])
 
 
 def sort_paths_numerically(paths: list[Path]) -> list[Path]:
@@ -27,21 +23,21 @@ def load_img_paths_from_dir(dir_path: Path):
     img_paths = list(sorted(dir_path.glob("*.jpg")))
     return img_paths
 
-def read_image(img_path: Path, pipeline_type: str = "base") -> Tensor:
-    img = tv.io.read_image(str(img_path))
 
-    
+def read_image(img_path: Path, pipeline_type: str = "base") -> Tensor:
+    img = torchvision.io.read_image(str(img_path))
+
     if pipeline_type == "base":
         img = base_img_pipeline(img)
 
     else:
         raise NotImplementedError
-    
+
     return img
 
 
 def save_image(save_path: Path, img: Tensor, jpg_quality: int = 100) -> None:
-    tv.io.write_jpeg(img, str(save_path), quality=jpg_quality)
+    torchvision.io.write_jpeg(img, str(save_path), quality=jpg_quality)
 
 
 def read_image_from_sample(data, sample_idx):
--- src/diffusion.py
+++ src/diffusion.py
@@ -2,8 +2,10 @@ from collections.abc import Iterable
 from dataclasses import dataclass
 from pathlib import Path
 
-from PIL import Image
 import torch
+import torchvision.transforms.v2 as tvtf2
+from torch import Tensor
+
 from diffusers import StableDiffusionImg2ImgPipeline, StableDiffusionXLImg2ImgPipeline
 
 from src.utils import get_device
@@ -17,25 +19,30 @@ class ModelId:
     sdxl_ref_v1_0 = "stabilityai/stable-diffusion-xl-refiner-1.0"
 
 
-sd_models = {
-    ModelId.sd_v1_5,
-    ModelId.sd_v2_1
-}
+sd_models = {ModelId.sd_v1_5, ModelId.sd_v2_1}
 
-sdxl_models = {
-    ModelId.sdxl_ref_v1_0
-}
+sdxl_models = {ModelId.sdxl_ref_v1_0}
 
 
 class ImageToImageDiffusionModel:
-    def __init__(self, model_id: str = ModelId.sdxl_ref_v1_0, device_name: str = None, low_mem_mode: bool = False) -> None:
+    def __init__(
+        self,
+        model_id: str = ModelId.sdxl_ref_v1_0,
+        device_name: str = None,
+        low_mem_mode: bool = False,
+        post_processing=None,
+    ) -> None:
         device = get_device() if device_name is None else torch.device(device_name)
-        
+
         if model_id in sd_models:
-            pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id, torch_dtype=torch.float16, variant="fp16")
+            pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
+                model_id, torch_dtype=torch.float16, variant="fp16"
+            )
 
         elif model_id in sdxl_models:
-            pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(model_id, torch_dtype=torch.float16, variant="fp16")
+            pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
+                model_id, torch_dtype=torch.float16, variant="fp16"
+            )
 
         else:
             raise NotImplementedError
@@ -45,22 +52,36 @@ class ImageToImageDiffusionModel:
 
         else:
             pipe = pipe.to(device)
-        
+
+        if post_processing is None:
+            post_processing = tvtf2.Compose([tvtf2.PILToTensor()])
+
         self.pipe = pipe
         self.device = device
+        self.post_processing = post_processing
 
-    def forward(self, image: Image, prompt: str = "", strength: float=0.1, guidance_scale: float=0.0, num_steps: int = 50):
+    def forward(
+        self,
+        image: Tensor,
+        prompt: str = "",
+        strength: float = 0.1,
+        guidance_scale: float = 0.0,
+        num_steps: int = 50,
+    ):
         img = self.pipe(
             image=image,
             prompt=prompt,
             strength=strength,
             guidance_scale=guidance_scale,
-            num_inference_steps=num_steps
-        )
-        return img.images[0]
+            num_inference_steps=num_steps,
+        ).images[0]
+        img = self.post_processing(img)
+        return img
 
 
-def diffuse_images_to_dir(model: ImageToImageDiffusionModel, img_paths: Iterable[Path], dst_dir: Path):
+def diffuse_images_to_dir(
+    model: ImageToImageDiffusionModel, img_paths: Iterable[Path], dst_dir: Path
+):
     dst_dir.mkdir(exist_ok=True, parents=True)
     for src_img_path in img_paths:
         src_img = read_image(src_img_path)
