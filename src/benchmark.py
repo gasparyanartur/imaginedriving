4f04517f976c738d172e2a334679d3e28b80a95a
Artur Gasparyan <gasparyanartur99@gmail.com>
2024-03-09 12:58:10 +0100

    Update benchmark code

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,55 +1,87 @@
 import pathlib as pl
+from pathlib import Path
 from collections.abc import Iterable
+
+import torch
+from torch import Tensor
 from torchmetrics.image import PeakSignalNoiseRatio, StructuralSimilarityIndexMeasure
 from torchmetrics.image.fid import FrechetInceptionDistance
-
 from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
 
-from src.data import read_image
+from src.data import read_image, load_img_if_path
 
-metric_names_mapping = {
-    "psnr": PeakSignalNoiseRatio(data_range=1.0),
-    "ssim": StructuralSimilarityIndexMeasure(data_range=1.0),
-    "lpips": LearnedPerceptualImagePatchSimilarity(net_type="squeeze"),
-    "fid": FrechetInceptionDistance(feature=64),
+
+def benchmark_fid(fid_metric, pred: Tensor, gt: Tensor) -> Tensor:
+    fid_metric.reset()
+    fid_metric.update(pred, real=False)
+    fid_metric.update(gt, real=True)
+    fid = fid_metric.compute()
+    return fid
+
+default_metrics = None
+
+fid_metric = FrechetInceptionDistance(feature=64)
+psnr_metric = PeakSignalNoiseRatio(data_range=1.0)
+ssim_metric = StructuralSimilarityIndexMeasure(data_range=1.0)
+lpips_metric = LearnedPerceptualImagePatchSimilarity(net_type="squeeze")
+
+default_funcs = {
+    "psnr": psnr_metric.__call__,
+    "ssim": ssim_metric.__call__,
+    "lpips": lpips_metric.__call__,
+    "fid": lambda pred, gt: benchmark_fid(fid_metric, pred, gt),
 }
 
 
-def benchmark_img_to_img(
-    img_pred_path: pl.Path, img_gt_path: pl.Path, metric_names: Iterable[str]
-) -> dict[str, float]:
+class Metrics:
+    def __init__(self, metric_funcs = None) -> None:
+        if metric_funcs is None:
+            metric_funcs = default_funcs
 
-    img_pred = read_image(img_pred_path)[None, ...]
-    img_gt = read_image(img_gt_path)[None, ...]
+        self.funcs = metric_funcs
 
-    metrics = {}
+    @property
+    def default(self):
+        return default_metrics
 
-    for metric_name in metric_names:
-        if metric_name in metric_names_mapping:
-            if metric_name == "fid":
-                metric_names_mapping[metric_name].update(img_pred, real=False)
-                metric_names_mapping[metric_name].update(img_gt, real=True)
-                metrics[metric_name] = metric_names_mapping[metric_name].compute()
+    @property
+    def metric_names(self):
+        return list(self.funcs.keys())
 
-            else:
-                metrics[metric_name] = metric_names_mapping[metric_name](
-                    img_pred, img_gt)
+    def benchmark_metric(self, metric_name: str, pred: Tensor, gt: Tensor):
+        if not metric_name in self.metric_names:
+            raise ValueError(f"Metric {metric_name} not found in metrics {self.metric_names}")
 
-    return metrics
+        with torch.no_grad():
+            return self.funcs[metric_name](pred, gt)
+
+    def benchmark(self, pred: Tensor | Path | str, gt: Tensor | Path | str, metric_names: Iterable[str] = None):
+        pred = load_img_if_path(pred)
+        gt = load_img_if_path(gt)
+            
+        if metric_names is None:
+            metric_names = self.metric_names
+
+        elif isinstance(metric_names, str):
+            metric_names = [metric_names]
+
+        return {metric_name: self.benchmark_metric(metric_name, pred, gt) for metric_name in metric_names}
+
+    def benchmark_dir_to_dir(
+        self, pred_dir_path: Path, gt_dir_path: Path, metric_names: str | Iterable[str] = None
+    ) -> dict[str, float]:
+        img_name_to_pred_path = {}
+        for path in pred_dir_path.glob("*.jpg"):
+            img_name_to_pred_path[path.stem] = path
+
+        benchmarks = {}
+        for gt_path in filter(lambda p: p.stem in img_name_to_pred_path, gt_dir_path.glob("*.jpg")):
+            img_name = gt_path.stem
+            pred_path = img_name_to_pred_path[img_name]
+            metric = self.benchmark(pred_path, gt_path, metric_names)
+            benchmarks[img_name] = metric
+
+        return benchmarks
 
 
-def benchmark_dir_to_dir(
-    pred_dir_path: pl.Path, gt_dir_path: pl.Path, metric_names: Iterable[str]
-) -> dict[str, float]:
-    pred_paths = {}
-    benchmarks = {}
-    for path in pred_dir_path.glob("*.jpg"):
-        pred_paths[path.stem] = path
-
-    for path in gt_dir_path.glob("*.jpg"):
-        if path.stem in pred_paths:
-            benchmarks[path.stem] = benchmark_img_to_img(
-                path, pred_paths[path.stem], metric_names
-            )
-
-    return benchmarks
+default_metrics = Metrics()
