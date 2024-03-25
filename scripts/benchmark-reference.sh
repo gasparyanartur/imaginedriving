ac1d5baa02d178e3097d32aa0dc6df8152a68744
Artur Gasparyan <gasparyanartur99@gmail.com>
2024-03-25 07:37:58 +0100

    Clear up dir

--- scripts/benchmark-reference.sh
+++
@@ -1,8 +0,0 @@
-index_array=(0 1 2 3 4)
-render_name_array=("0m" "2m" "4m" "6m" "8m")
-
-for i in ${index_array[@]}
-do
-    render_name=${render_name_array[$i]}
-    python "./scripts/run_benchmarks.py" reference/pandaset-01/renders/${render_name} reference/pandaset-01/renders/gt --save_dir reference/pandaset-01/metrics/renders-${render_name}-vs-gt
-done
