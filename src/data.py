699d363ad4730fbf2594b3f496f18a86140d9eac
Artur Gasparyan <gasparyanartur99@gmail.com>
2024-03-26 17:48:04 +0100

    implemented dynamic dataset

--- src/data.py
+++ src/data.py
@@ -1,5 +1,7 @@
+from typing import Any
 from abc import ABC, abstractmethod
-from collections.abc import Iterable
+from dataclasses import dataclass
+from collections.abc import Iterable, Generator
 from pathlib import Path
 import json
 
@@ -9,9 +11,11 @@ from torch import Tensor
 from torchvision.transforms import v2 as tvtf2
 import torchvision
 
+from src.configuration import read_yaml
+
 
 base_img_pipeline = tvtf2.Compose([tvtf2.ToDtype(torch.float32, scale=True)])
-
+pandaset_img_suffix = ".jpg"
 
 def img_float_to_img(img: Tensor):
     img = img * 255
@@ -72,7 +76,7 @@ def iter_numeric_names(start_name: str | int, end_name: str | int, fixed_len: in
         yield num
 
 
-def expand_data_tree(data_tree, scene_name_len: int = 2, sample_name_len: int = 2):
+def expand_data_tree(data_tree, scene_name_len: int = 3, sample_name_len: int = 2):
     dataset_dict = {}
     for dataset_name, dataset in data_tree.items():
         scene_dict = {}
@@ -259,48 +263,80 @@ class PandasetDataset(Dataset):
         }
 
 
-class PathBuilder(ABC):
-    @abstactmethod
-    def get_sample_path(self, dataset_path: Path, dataset: str, scene: str, sample: str) -> Path:
+class DataGetter(ABC):
+    @abstractmethod
+    def get_sample_path(self, dataset_path: Path, scene: str, sample: str) -> Path:
         raise NotImplementedError
 
     @abstractmethod
-    def iterate_samples_in_scene(self, dataset_path: Path, dataset: str, scene: str) -> Generator[Path]:
+    def iterate_samples_in_scene(self, dataset_path: Path, scene: str) -> Generator[Path]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def load_data(self, path: Path):
         raise NotImplementedError
 
     def paths_from_tree(self, dataset_path: Path, expanded_tree: dict[str, any]) -> list[Path]:
         samples = []
-        for dataset_name, dataset_dict in tree.items():
+        for _, dataset_dict in expanded_tree.items():
             for scene_name, sample_list in dataset_dict.items():
                 if sample_list is None:
-                    samples.extend(self.iterate_samples_in_scene(dataset_path, dataset_name, scene_name))
+                    samples.extend(self.iterate_samples_in_scene(dataset_path, scene_name))
                     continue
 
                 for sample_name in sample_list:
-                    samples.append(self.get_sample_path(dataset_name, scene_name, sample_name))
+                    samples.append(self.get_sample_path(dataset_path, scene_name, sample_name))
         return samples
 
-class PandasetImagePathBuilder(PathBuilder):
-    def __init__(self, camera: str = "front"):
-        ...
+
+class PandasetImageGetter(DataGetter):
+    def __init__(self, camera: str = "front_camera", tf_pipeline=base_img_pipeline):
+        self.camera = camera
+        self.tf_pipeline = tf_pipeline
+
+    def get_sample_path(self, dataset_path: Path, scene: str, sample: str) -> Path:
+        path = dataset_path / scene / "camera" / self.camera / sample
+        path = path.with_suffix(pandaset_img_suffix)
+        return path
+
+    def iterate_samples_in_scene(self, dataset_path: Path, scene: str) -> Generator[Path]:
+        cam_path = dataset_path / scene / "camera" / self.camera
+        return cam_path.glob(f"*{pandaset_img_suffix}")
+
+    def load_data(self, path: Path):
+        img = read_image(path, self.tf_pipeline)
+        return img
 
 
-    def get_sample_path(self, dataset_path: Path, dataset: str, scene: str, sample: str) -> Path:
-        return 
+class SampleLoader(ABC):
+    @abstractmethod
+    def load_sample(self, path, **kwargs):
+        raise NotImplementedError
 
 
-class DynamicDataset(Dataset):
-    # Dataset / Scene / Sample
-    def __init__(self, dataset_path: Path, data_tree: dict[str, any], path_builders: dict[str, PathBuilder]):
+class DynamicDataset(Dataset):      # Dataset / Scene / Sample
+    def __init__(self, dataset_path: Path, tree_path: Path, data_getters: dict[str, DataGetter]):
+        data_tree = read_yaml(tree_path)
         expanded_tree = expand_data_tree(data_tree)
         self.sample_paths = {
-            name: builder.paths_from_tree(dataset_path, expanded_tree) for name, builder in path_builders.items()
+            name: getter.paths_from_tree(dataset_path, expanded_tree) for name, getter in data_getters.items()
         }
+        self.sample_names = list(self.sample_paths.keys())
+        self.data_getters = data_getters
 
     def __len__(self) -> int:
         example_paths = next(self.sample_paths.values())
         return len(example_paths)
 
-    def __getitem__(self, idx):
-        sample = {} # TODO: Fix sample with loaders
+    def __getitem__(self, idx: int) -> dict[str, Any]:
+        sample = {} 
+
+        for name in self.sample_names:
+            getter = self.data_getters[name]
+            path = self.sample_paths[name][idx]
+            data = getter.load_data(path)
+
+            sample[name] = data
+
+        return sample
 
