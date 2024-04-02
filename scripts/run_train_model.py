105869a52012118dbc73299225fb8557bf1c32c6
Artur Gasparyan <gasparyanartur99@gmail.com>
2024-04-03 01:22:58 +0200

    Wip: add code

--- scripts/run_train_model.py
+++ scripts/run_train_model.py
@@ -1,3 +1,4 @@
+from typing import Any
 from pathlib import Path
 from argparse import ArgumentParser
 import logging
@@ -8,11 +9,34 @@ import transformers
 from transformers import AutoTokenizer, AutoModelForTextEncoding
 from transformers import AutoTokenizer, PretrainedConfig
 import diffusers
-from diffusers.utils import check_min_version, is_wandb_available
+from diffusers import (
+    AutoencoderKL,
+    DDPMScheduler,
+    StableDiffusionXLPipeline,
+    UNet2DConditionModel,
+)
+from diffusers.loaders import LoraLoaderMixin
+from diffusers.optimization import get_scheduler
+from diffusers.training_utils import _set_state_dict_into_text_encoder, cast_training_params, compute_snr
+from diffusers.utils import (
+    check_min_version,
+    convert_state_dict_to_diffusers,
+    convert_unet_state_dict_to_peft,
+    is_wandb_available,
+)
+from diffusers.utils.hub_utils import load_or_create_model_card, populate_model_card
+from diffusers.utils.import_utils import is_xformers_available
+from diffusers.utils.torch_utils import is_compiled_module
+
+
 import datasets
 from accelerate import Accelerator
 from accelerate.logging import get_logger
-from accelerate.utils import ProjectConfiguration, set_seed
+from accelerate.utils import (
+    DistributedDataParallelKwargs,
+    ProjectConfiguration,
+    set_seed,
+)
 from huggingface_hub import create_repo, upload_folder
 import wandb
 
@@ -22,7 +46,6 @@ from src.data import setup_project
 check_min_version("0.23.0.dev0")
 
 logger = get_logger(__name__)
-
 
 
 def import_model_class_from_model_name_or_path(
@@ -45,14 +68,11 @@ def import_model_class_from_model_name_or_path(
         raise ValueError(f"{model_class} is not supported.")
 
 
-
-
 def parse_args():
     parser = ArgumentParser(
         "train_model", description="Finetune a given model on a dataset"
     )
     parser.add_argument("config_path", type=Path)
-
 
     args = parser.parse_args()
     return args
@@ -150,25 +170,49 @@ def main(args):
     # ===   Setup script   ===
     # ========================
 
-
     config_path = args.config_path
     config = setup_project(config_path)
 
     output_dir = Path(config["output_dir"])
     logging_dir = output_dir / "logs"
 
-    model_config = config["model"]
+    model_config: dict[str, Any] = config["model"]
+    report_to = model_config.get("report_to")
+    mixed_precision = model_config.get("mixed_precision")
+    log_with = model_config.get("log_with")
+    revision = model_config.get("revision")
+    variant = model_config.get("variant")
+
+    # Sanity checks
+    if report_to == "wandb" and model_config.get("hub_token") is not None:
+        raise ValueError(
+            "You cannot use both --report_to=wandb and --hub_token due to a security risk of exposing your token."
+            " Please use `huggingface-cli login` to authenticate with the Hub."
+        )
+
+    if report_to == "wandb":
+        assert is_wandb_available()
+
+    if torch.backends.mps.is_available() and mixed_precision == "bf16":
+        # due to pytorch#99272, MPS does not yet support bfloat16.
+        raise ValueError(
+            "Mixed precision training with bfloat16 is not supported on MPS. Please use fp16 (recommended) or fp32 instead."
+        )
+
+    # Setup accelerator
 
     accelerator_project_config = ProjectConfiguration(
         project_dir=output_dir, logging_dir=logging_dir
     )
+    accelerator_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
     accelerator = Accelerator(
-        mixed_precision=model_config["mixed_precision"],
-        log_with=model_config["log_with"],
-        project_config=accelerator_project_config
+        mixed_precision=mixed_precision,
+        log_with=log_with,
+        project_config=accelerator_project_config,
+        kwargs_handlers=[accelerator_kwargs],
     )
 
-   # Make one log on every process with the configuration for debugging.
+    # Make one log on every process with the configuration for debugging.
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
         datefmt="%m/%d/%Y %H:%M:%S",
@@ -194,24 +238,39 @@ def main(args):
         logging_dir.mkdir(exist_ok=True)
 
         if model_config["push_to_hub"]:
-            #repo_id = create_repo(
+            # repo_id = create_repo(
             #    repo_id=args.hub_model_id or Path(args.output_dir).name, exist_ok=True, token=args.hub_token
-            #).repo_id
+            # ).repo_id
             raise NotImplementedError
-
-    # Sanity checks
-    assert is_wandb_available()
 
     # =======================
     # ===   Load models   ===
     # =======================
 
     model_id = model_config["pretrained_model_name_or_path"]
+
     tokenizer_one = AutoTokenizer.from_pretrained(
-        model_id, subfolder="tokenizer", use_fast=False
+        model_id, subfolder="tokenizer", revision=revision, use_fast=False
     )
     tokenizer_two = AutoTokenizer.from_pretrained(
-        model_id, subfolder="tokenizer_2", use_fast=False
+        model_id, subfolder="tokenizer_2", revision=revision, use_fast=False
+    )
+
+    # import correct text encoder classes
+    text_encoder_cls_one = import_model_class_from_model_name_or_path(
+        model_id, revision, subfolder="text_encoder"
+    )
+    text_encoder_cls_two = import_model_class_from_model_name_or_path(
+        model_id, revision, subfolder="text_encoder_2"
+    )
+
+    # Load scheduler and models
+    noise_scheduler = DDPMScheduler.from_pretrained(model_id, subfolder="scheduler")
+    text_encoder_one = text_encoder_cls_one.from_pretrained(
+        model_id, subfolder="text_encoder", revision=revision, variant=variant
+    )
+    text_encoder_two = text_encoder_cls_two.from_pretrained(
+        model_id, subfolder="text_encoder_2", revision=revision, variant=variant
     )
 
     # ======================
@@ -225,7 +284,7 @@ def main(args):
     # =======================
     # ===   Train model   ===
     # =======================
-    
+
     # TODO
     ...
 
